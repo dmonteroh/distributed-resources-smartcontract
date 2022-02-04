89955b487f77e6c420165c7447a10ce6bfe18d8b
Daniel Montero <dmontero93@gmail.com>
2022-02-04 16:20:45 +0200

    Basic SmartContract : Read & Create Stat Asset

--- chaincode/smartcontract.go
+++ chaincode/smartcontract.go
@@ -15,10 +15,10 @@ type SmartContract struct {
 
 // InitLedger adds a base set of assets to the ledger
 func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) error {
-	stats := []internal.StatsObject{}
+	stats := []internal.StoredStat{}
 
 	for _, stat := range stats {
-		err := ctx.GetStub().PutState(stat.Identity.IP, []byte(stat.String()))
+		err := ctx.GetStub().PutState(stat.DrcHost.HostID, []byte(stat.String()))
 		if err != nil {
 			return fmt.Errorf("failed to put to world state. %v", err)
 		}
@@ -28,19 +28,25 @@ func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) 
 }
 
 // CreateAsset issues a new asset to the world state with given details.
-func (s *SmartContract) CreateStat(ctx contractapi.TransactionContextInterface, statIP string, statJSON string) error {
-	exists, err := s.StatExists(ctx, statIP)
+func (s *SmartContract) CreateAsset(ctx contractapi.TransactionContextInterface, statIP string, statJSON string) error {
+	exists, err := s.AssetExists(ctx, statIP)
 	if err != nil {
 		return err
 	}
 	if exists {
 		return fmt.Errorf("the Stats for %s already exists", statIP)
 	}
-	return ctx.GetStub().PutState(statIP, []byte(statJSON))
+
+	tmpStat := internal.DrcJsonToStruct(statJSON)
+	toStore := internal.ConvertToStorage(tmpStat)
+	toStore.ID = statIP
+	// RUN VALIDATION
+
+	return ctx.GetStub().PutState(statIP, []byte(toStore.String()))
 }
 
 // ReadAsset returns the asset stored in the world state with given id.
-func (s *SmartContract) ReadStat(ctx contractapi.TransactionContextInterface, statIP string) (*internal.StatsObject, error) {
+func (s *SmartContract) ReadAsset(ctx contractapi.TransactionContextInterface, statIP string) (*internal.StoredStat, error) {
 	statJSON, err := ctx.GetStub().GetState(statIP)
 	if err != nil {
 		return nil, fmt.Errorf("failed to read from world state: %v", err)
@@ -49,7 +55,7 @@ func (s *SmartContract) ReadStat(ctx contractapi.TransactionContextInterface, st
 		return nil, fmt.Errorf("the Stats for %s do not exist", statIP)
 	}
 
-	var stat internal.StatsObject
+	var stat internal.StoredStat
 	err = json.Unmarshal(statJSON, &stat)
 	if err != nil {
 		return nil, err
@@ -59,32 +65,37 @@ func (s *SmartContract) ReadStat(ctx contractapi.TransactionContextInterface, st
 }
 
 // UpdateAsset updates an existing asset in the world state with provided parameters.
-func (s *SmartContract) UpdateStat(ctx contractapi.TransactionContextInterface, statIP string, statJSON string) error {
-	exists, err := s.StatExists(ctx, statIP)
+func (s *SmartContract) UpdateAsset(ctx contractapi.TransactionContextInterface, statIP string, statJSON string) error {
+	exists, err := s.AssetExists(ctx, statIP)
 	if err != nil {
 		return err
 	}
 	if !exists {
 		return fmt.Errorf("the Stats for %s do not exist", statIP)
 	}
-	return ctx.GetStub().PutState(statIP, []byte(statJSON))
+
+	tmpStat := internal.DrcJsonToStruct(statJSON)
+	toStore := internal.ConvertToStorage(tmpStat)
+	toStore.ID = statIP
+
+	return ctx.GetStub().PutState(statIP, []byte(toStore.String()))
 }
 
 // DeleteAsset deletes an given asset from the world state.
-func (s *SmartContract) DeleteLanding(ctx contractapi.TransactionContextInterface, dronename string) error {
-	exists, err := s.StatExists(ctx, dronename)
+func (s *SmartContract) DeleteAsset(ctx contractapi.TransactionContextInterface, statIP string) error {
+	exists, err := s.AssetExists(ctx, statIP)
 	if err != nil {
 		return err
 	}
 	if !exists {
-		return fmt.Errorf("the Stats for %s do not exist", dronename)
+		return fmt.Errorf("the Stats for %s do not exist", statIP)
 	}
 
-	return ctx.GetStub().DelState(dronename)
+	return ctx.GetStub().DelState(statIP)
 }
 
 // AssetExists returns true when asset with given ID exists in world state
-func (s *SmartContract) StatExists(ctx contractapi.TransactionContextInterface, statIP string) (bool, error) {
+func (s *SmartContract) AssetExists(ctx contractapi.TransactionContextInterface, statIP string) (bool, error) {
 	statJSON, err := ctx.GetStub().GetState(statIP)
 	if err != nil {
 		return false, fmt.Errorf("failed to read from world state: %v", err)
@@ -94,19 +105,19 @@ func (s *SmartContract) StatExists(ctx contractapi.TransactionContextInterface, 
 }
 
 // TransferAsset updates the owner field of asset with given id in world state.
-func (s *SmartContract) TransferStat(ctx contractapi.TransactionContextInterface, statIP string, newStatIP string) (string, error) {
-	statObject, err := s.ReadStat(ctx, statIP)
+func (s *SmartContract) TransferAsset(ctx contractapi.TransactionContextInterface, statIP string, newStatIP string) (string, error) {
+	statObject, err := s.ReadAsset(ctx, statIP)
 	if err != nil {
 		return "", err
 	}
 
-	statObject.Identity.IP = newStatIP
+	statObject.ID = newStatIP
 
 	return statObject.String(), ctx.GetStub().PutState(newStatIP, []byte(statObject.String()))
 }
 
 // GetAllAssets returns all assets found in world state
-func (s *SmartContract) GetAllAssets(ctx contractapi.TransactionContextInterface) ([]*internal.StatsObject, error) {
+func (s *SmartContract) GetAllAssets(ctx contractapi.TransactionContextInterface) ([]*internal.StoredStat, error) {
 	// range query with empty string for startKey and endKey does an
 	// open-ended query of all assets in the chaincode namespace.
 	resultsIterator, err := ctx.GetStub().GetStateByRange("", "")
@@ -115,14 +126,14 @@ func (s *SmartContract) GetAllAssets(ctx contractapi.TransactionContextInterface
 	}
 	defer resultsIterator.Close()
 
-	var statObjects []*internal.StatsObject
+	var statObjects []*internal.StoredStat
 	for resultsIterator.HasNext() {
 		queryResponse, err := resultsIterator.Next()
 		if err != nil {
 			return nil, err
 		}
 
-		var statObject internal.StatsObject
+		var statObject internal.StoredStat
 		err = json.Unmarshal(queryResponse.Value, &statObject)
 		if err != nil {
 			return nil, err
